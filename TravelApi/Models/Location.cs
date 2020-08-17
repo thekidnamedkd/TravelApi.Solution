d5afb8c7dd1160fb005c31fbc084d09b4a8bf6bc
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-17 10:42:18 -0700

    add the TravelApi Context and the Location class and constructor
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ TravelApi/Models/Location.cs
@@ -0,0 +1,10 @@
+namespace TravelApi.Models
+{
+  public class Location
+  {
+    public int LocationId   {get; set;}
+    public string City  {get; set;}
+    public string Country { get; set; }
+    public string Continent { get; set; }
+  }
+}
