ec4e770a58f2b76002129fee424459f80d3a369c
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-18 16:11:37 -0700

    final commit for today
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ TravelApi/Migrations/20200818220803_Mexico_City.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Migrations
+{
+    public partial class Mexico_City : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3,
+                column: "City",
+                value: "Mexico_City");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3,
+                column: "City",
+                value: "Mexico City");
+        }
+    }
+}
