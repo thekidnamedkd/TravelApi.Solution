b04985df687eeb76011a2f507fdae2ed07e73733
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-19 14:56:40 -0700

    reboot code and pull from repo to revert to previous feature
    Co-Authored-By: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ TravelApi/Migrations/20200819215102_Pagination2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Migrations
+{
+    public partial class Pagination2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
